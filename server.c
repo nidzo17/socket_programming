1025de24cf67847882919ef00c9d76ca20262355
Nikola <nikola.ravnjak@gmail.com>
2015-04-09 00:16:06 +0200

    * indent fix

--- server.c
+++ server.c
@@ -10,13 +10,13 @@
 
 int main() {
     int listen_fd1, listen_fd2, connection_fd1, connection_fd2;
-	int PORT1 = 50007;
+    int PORT1 = 50007;
     int PORT2 = 53007;
     struct sockaddr_in serv_addr1, serv_addr2, client_addr; 
-	socklen_t client_length;
+    socklen_t client_length;
     char buffer[1024] = {0};
 	
-	// SOCKET 1
+    // SOCKET 1
     listen_fd1 = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr1, 0, sizeof(serv_addr1));
 	
@@ -36,30 +36,28 @@ int main() {
     serv_addr2.sin_port = htons(PORT2);
     
     bind(listen_fd2, (struct sockaddr*)&serv_addr2, sizeof(serv_addr2)); 
-	listen(listen_fd2, 1); 
+    listen(listen_fd2, 1); 
 	
-	client_length = sizeof(client_addr);
-
+    client_length = sizeof(client_addr);
 	
     while(1) {
-		connection_fd1 = accept(listen_fd1, (struct sockaddr*) &client_addr, &client_length); 
-		connection_fd2 = accept(listen_fd2, (struct sockaddr*) &client_addr, &client_length); 
-		if (read(connection_fd1, buffer, sizeof(buffer) - 1) < 0) {
-			break;
-		}
-		printf("Received from 1st socket: %s\n", buffer);
+        connection_fd1 = accept(listen_fd1, (struct sockaddr*) &client_addr, &client_length); 
+	connection_fd2 = accept(listen_fd2, (struct sockaddr*) &client_addr, &client_length); 
+	if (read(connection_fd1, buffer, sizeof(buffer) - 1) < 0)
+	    break;
+	printf("Received from 1st socket: %s\n", buffer);
 		
-		printf("Sending to 2nd socket: %s\n", buffer);
-		if (write(connection_fd2, buffer, strlen(buffer)) < 0) {
-			fprintf(stderr, "ERROR: Writing to socket failed.\n");
-			return 1;
-		}
-		memset(&buffer, 0, sizeof(buffer)); 
-		close(connection_fd1);
-		close(connection_fd2);
-		sleep(1);
+	printf("Sending to 2nd socket: %s\n", buffer);
+	if (write(connection_fd2, buffer, strlen(buffer)) < 0) {
+	    fprintf(stderr, "ERROR: Writing to socket failed.\n");
+	    return 1;
 	}
 	
-	return 0;
+        memset(&buffer, 0, sizeof(buffer)); 
+	close(connection_fd1);
+	close(connection_fd2);
+	sleep(1);
+    }	
+    return 0;
 }
 
